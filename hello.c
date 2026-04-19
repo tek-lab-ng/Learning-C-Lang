86ca9e899159f66eced3a6c2ae26948f00384ad4
Gabriel <gojabriel85@gmail.com>
2026-04-19 13:41:11 +0100

    I worked with input/output path of the C program

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 # include <stdio.h>
 
 int main (void)
-{ printf("My first on the C language\n");
+{ 
+	char f, m, l;
+	int age;
+
+	printf("Please Type in your 3 credential letter for your name follow by age: ");
+	scanf("%c%c%c%d", &f,&m,&l,&age);
+	printf("This are my initials: %c%c%c and my age is: %d. \n", f,m,l, 30);
+		return (0);
 }
